50d7e75f5e61d329c4a5bfd47fc44821392a6e26
hotaz <wizhotaz@gmail.com>
2022-10-13 11:47:17 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
 /**
  *
  *    main -Entry point
- *
  *   
- *
  *    Return: Always 0 (Success)
- *
  */
 
 int main(void)
@@ -16,6 +12,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 
 }
